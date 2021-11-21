eddc1d89e9f5178aec44158aeb96a795883b761c
SzymonLukasik <szymon123lukasik@gmail.com>
2021-11-21 03:55:59 +0100

    Moved validate.* files to directory

--- src/main/java/concurrentcube/Cube.java
+++ src/main/java/concurrentcube/Cube.java
@@ -1,2 +1,20 @@
-package concurrentcube;public class Cube {
+package concurrentcube;
+
+import java.util.function.BiConsumer;
+
+public class Cube {
+
+    public Cube(int size,
+                BiConsumer<Integer, Integer> beforeRotation,
+                BiConsumer<Integer, Integer> afterRotation,
+                Runnable beforeShowing,
+                Runnable afterShowing) {
+    }
+
+    public void rotate(int side, int layer) throws InterruptedException {
+    }
+
+    public String show() throws InterruptedException {
+    }
+
 }
